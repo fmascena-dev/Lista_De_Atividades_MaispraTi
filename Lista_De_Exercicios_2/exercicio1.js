7924183d144ed3f9fe5e7371af63fc47bd6d6d0d
felipe mascena <felipe.mascena.dev@gmail.com>
2025-05-06 00:53:15 -0300

    feat: finalizando o exercício 2

--- Lista_De_Exercicios_2/exercicio1.js
+++ Lista_De_Exercicios_2/exercicio1.js
@@ -1,3 +1,5 @@
+// 1. Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule quantos dias de vida um fumante perderá e exiba o total em dias.
+
 const prompt = require('prompt-sync')();
 
 const cigarrosPorDia = parseFloat(prompt('Quantos cigarros você fuma por dia? '));
---
+++ Lista_De_Exercicios_2/exercicio2.js
@@ -0,0 +1,12 @@
+// 2. Escreva um programa que pergunte a velocidade de um carro. Caso ultrapasse 80 Km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$ 5,00 por cada Km acima da velocidade permitida.
+
+const prompt = require('prompt-sync')();
+
+const velocidade = parseFloat(prompt("Qual a velocidade do caro (km/h)? "))
+
+if (velocidade > 80) {
+    let multa = (velocidade - 80) * 5
+    console.log(`Você foi multado! O valor da multa é: R$ ${multa.toFixed(2)}`)
+} else {
+    console.log("Você está dentro do limite de velocidade permitido!")
+}
